df446982ac219c096788b918c64684ffde9ce97b
OwlBob <tarakankist@yandex.ru>
2023-06-28 22:10:35 +0300

    update gendiff.js, package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.1');
+  .version('1.0.3')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>');
 
 program.parse();
